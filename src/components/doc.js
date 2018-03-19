a5d27956e8de05c0919165bae5699f3c9c675363
CharlyCast <charly.castes@polytechnique.edu>
2018-03-19 09:00:08 +0100

    Writing more doc

--- src/components/doc.js
+++ src/components/doc.js
@@ -4,7 +4,7 @@ export default class Doc extends Component {
     render() {
         return (
             <div>
-                <h3>General</h3>
+                <h2>General</h2>
 
                 <p>
                     Colorful is a symbolic interpreted language designed to be used on portable device with
@@ -12,9 +12,9 @@ export default class Doc extends Component {
                     name so that it is no more needed to type anything with a keyboard.
                 </p>
 
-                <h3>Basic symbols</h3>
+                <h2>Basic symbols</h2>
 
-                <h4>Numbers and boolean</h4>
+                <h3>Numbers and boolean</h3>
 
                 <p>
                     <img src={"img/doc/numbers.png"}/> Numbers are basics symbols that can be used to specify a value
@@ -32,14 +32,41 @@ export default class Doc extends Component {
 
                 <p>
                     <img src={"img/doc/variable.png"}/> Variable can be call with the triangle symbol, they should
-                    always have a color other than white, this is what replace the name in Colorful.
-                    Calling a variable this way result in the exact same way as writing its name in any other language.
+                    always have a color other than white, this is what replace names in Colorful.
+                    Calling a variable this way result the exact same way as writing its name in any other language does.
                 </p>
                 <p>
                     There are two ways to assign a value to a variable. For any complex expression it should be done
                     with an arrow (the assign symbol) of the corresponding color followed by the expression to be
                     evaluated. For a simple assignment such as a numerical or boolean value it is easier to just
                     write the value in the right color, the corresponding variable will be assigned with this value.
+                </p>
+                <p>
+                    <img src={"img/doc/assignArrow.png"}/> or <img src={"img/doc/assignDigit.png"}/> have the same
+                    result : they store the value 1 in the variable orange.
+                </p>
+
+                <h3>Operators</h3>
+                <p>
+                    <img src={"img/doc/operators.png"}/> Respectively the addition, subtraction, multiplication,
+                    division and modulo operators. A white operator behave as expected in any other language but a
+                    colored one will be interpreted as a white operator followed by the variable of the corresponding
+                    color.
+                </p>
+                <p>
+                    <img src={"img/doc/whitePlus.png"}/> and <img src={"img/doc/coloredPlus.png"}/> behave exactly in
+                    the same way. This could be generalized to most of colorful symbols..
+                </p>
+
+                <h3>Comparators</h3>
+                <p>
+                    <img src={"img/doc/comparators.png"}/> Comparators could also be either white or colored, in the
+                    later case they will be interpreted as if followed by the corresponding colored variable.
+                </p>
+
+                <h3>Boolean operators</h3>
+                <p>
+                    <img src={"img/doc/andOr.png"}/> Respectively "and" and "or" operators.
                 </p>
             </div>
         )
